362fadb8b3b972679c1ee96211125765b8f39e5f
Sushavon007 <sushavon098@gmail.com>
2025-06-30 13:47:59 +0530

    Recover functional code

--- src/Components/common/CreateButton.jsx
+++ src/Components/common/CreateButton.jsx
@@ -1,16 +1,12 @@
-import { Plus } from "lucide-react";
-import { useNavigate } from "react-router-dom";
+import { Plus } from "lucide-react"
+import { useNavigate } from "react-router-dom"
 
-function CreateButton({ label = "Create", url = "/", onClick }) {
-  const navigate = useNavigate();
+function CreateButton({ label = "Create", url = "/" }) {
+  const navigate = useNavigate()
 
   const handleClick = () => {
-    if (onClick) {
-      onClick(); // Open modal if passed
-    } else {
-      navigate(url); // Navigate if no onClick
-    }
-  };
+    navigate(url)
+  }
 
   return (
     <button
@@ -18,11 +14,9 @@ function CreateButton({ label = "Create", url = "/", onClick }) {
       className="w-full max-w-[250px] flex justify-center items-center bg-[#367aff] text-white p-1 rounded-xl hover:bg-blue-700 transition-colors inter"
     >
       <Plus className="h-9 w-9 bg-white text-[#367aff] rounded-lg p-1" />
-      <span className="mx-3 text-xs md:text-base whitespace-nowrap text-center flex-1">
-        {label}
-      </span>
+      <span className="mx-3 text-xs md:text-base whitespace-nowrap text-center flex-1">{label}</span>
     </button>
-  );
+  )
 }
 
 export default CreateButton;
