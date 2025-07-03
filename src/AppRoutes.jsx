64f2967c9f15fbfa2bf98c34cadceab0e0f11c36
Byte_Rohit <rohitneogi26042002@gmail.com>
2025-07-03 09:35:50 +0530

    Refactor authentication context and routing; simplify login state management and remove unnecessary cookie handling

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 
 // Layout
@@ -9,7 +9,7 @@ import SupAdminSignIn from "./pages/SuperAdmin/SuperAdminLogin";
 // import ModeratorLogin from "./pages/Auth/ModeratorLogin";
 import UserLogin from "./pages/User/userLogin";
 import UserRegister from "./pages/User/userRegister";
-import ResetPassword from "./pages/User/resetPassword";
+import ResetPassword from "./pages/User/resetPassword"
 
 // SuperAdmin pages
 import SuperAdminDashboard from "./pages/SuperAdmin/Dashboard";
@@ -28,15 +28,11 @@ import DepartmentAdminDashboard from "./pages/SuperAdmin/DepartAdminDashboard";
 import CreateJobForm from "./Components/superadmin/forms/CreateJobForm";
 import Notifications from "./Components/common/Notifications";
 import Settings from "./Components/common/Settings";
-
-// Login context
-import { useLogin } from "./contexts/SuperAdmin/LoginContext";  // Import useLogin hook
 import Applications from "./pages/User/Applications";
 import Profile from "./pages/User/Profile";
 
-function AppRoutes() {
-  const { isLogin, userRole } = useLogin(); 
 
+function AppRoutes() {
   return (
     <Routes>
       {/* Default route */}
@@ -44,17 +40,18 @@ function AppRoutes() {
 
       {/* Auth routes */}
       <Route path="/superadmin/login" element={<SupAdminSignIn />} />
+      {/* <Route path="/moderator/login" element={<ModeratorLogin />} /> */}
       <Route path="/user/login" element={<UserLogin />} />
       <Route path="/user/register" element={<UserRegister />} />
       <Route path="/user/resetpassword" element={<ResetPassword />} />
 
       {/* SuperAdmin routes */}
-      <Route path="/superadmin" element={isLogin && userRole === "superadmin" ? <Layout /> : <Navigate to="/superadmin/login" replace />}>
+      <Route path="/superadmin" element={<Layout />}>
         <Route path="dashboard" element={<SuperAdminDashboard />} />
-        <Route path="departments" element={userRole === "superadmin" ? <Departments /> : <Navigate to="/superadmin/login" replace />} />
-        <Route path="forms" element={userRole === "superadmin" ? <SuperAdminForms /> : <Navigate to="/superadmin/login" replace />} />
-        <Route path="forms/create-job" element={userRole === "superadmin" ? <CreateJobForm /> : <Navigate to="/superadmin/login" replace />} />
-        <Route path="dept_admin" element={userRole === "superadmin" ? <DepartmentAdminDashboard /> : <Navigate to="/superadmin/login" replace />} />
+        <Route path="departments" element={<Departments />} />
+        <Route path="forms" element={<SuperAdminForms />} />
+        <Route path="/superadmin/forms/create-job" element={<CreateJobForm />} />
+        <Route path="dept_admin" element={<DepartmentAdminDashboard />} />
         <Route path="notifications" element={<Notifications />} />
         <Route path="settings" element={<Settings />} />
         <Route path="support" element={<Support />} />
@@ -63,15 +60,15 @@ function AppRoutes() {
       </Route>
 
       {/* Moderator routes */}
-      <Route path="/moderator" element={isLogin && userRole === "moderator" ? <Layout /> : <Navigate to="/moderator/login" replace />}>
+      <Route path="/moderator" element={<Layout />}>
         <Route path="dashboard" element={<ModeratorDashboard />} />
       </Route>
 
       {/* User routes */}
-      <Route path="/user" element={isLogin && userRole === "user" ? <Layout /> : <Navigate to="/user/login" replace />}>
-      <Route path="applications" element={userRole === "user" ? <Applications /> : <Navigate to="/user/login" replace />} />
-      <Route path="profile" element={userRole === "user" ? <Profile /> : <Navigate to="/user/login" replace />} />
+      <Route path="/user" element={<Layout />}>
         <Route path="dashboard" element={<UserDashboard />} />
+        <Route path="applications" element={<Applications/>} />
+        <Route path="profile" element={<Profile />} />
         <Route path="notifications" element={<Notifications />} />
         <Route path="settings" element={<Settings />} />
         <Route path="support" element={<Support />} />
@@ -79,8 +76,8 @@ function AppRoutes() {
         <Route path="feedback" element={<Feedback />} />
       </Route>
 
-      {/* Catch-all route */}
-      <Route path="*" element={<Navigate to="/superadmin/login" replace />} />
+      {/* Catch-all */}
+      <Route path="*" element={<Navigate to="/user/login" replace />} />
     </Routes>
   );
 }
